6300e1053d5a55280523d287c799acdd362c4dce
PandoraLH <lixhen2002@gmail.com>
2023-05-16 23:47:52 +0700

    refacto

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../utils/theme";
+import Layout from "@/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
+   const pageTitle = pageProps.title || ""; // Default empty string if no title is provided
+
    return (
-      <ThemeProvider theme={theme}>
-         <Component {...pageProps} />
-      </ThemeProvider>
+      <div className="flex items-center justify-center">
+         <div className="w-[1152px] border-l border-r border-gray-300">
+            <ThemeProvider theme={theme}>
+               <Layout title={pageTitle}>
+                  <Component {...pageProps} title={pageTitle} />
+               </Layout>
+            </ThemeProvider>
+         </div>
+      </div>
    );
 }
 
--- pages/series/[seriesId].js
+++ pages/series/[seriesId].js
@@ -1,60 +1,82 @@
-import axios from 'axios';
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 export default function SeriesPage({ book }) {
-  if (!book) {
-    return <p>Loading...</p>;
-  }
+   const router = useRouter();
 
-  return (
-    <div>
-      <h1>{book.name}</h1>
-      <p>{book.href}</p>
-      {/* Add more book details as needed */}
-    </div>
-  );
+   useEffect(() => {
+      if (!book) {
+         router.push("/404"); // Redirect to a 404 page if book data is not available
+      }
+   }, [book, router]);
+
+   if (!book) {
+      return (
+         <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
+         >
+            <CircularProgress />
+         </Box>
+      );
+   }
+
+   return (
+      <Box className="flex justify-center">
+         <Box maxWidth="1152px" mx="auto" p={4}>
+            Test
+         </Box>
+      </Box>
+   );
 }
 
 export async function getStaticPaths() {
-  try {
-    const response = await axios.get('http://localhost:3000/api/book/getAll');
-    const books = response.data;
+   try {
+      const response = await axios.get("http://localhost:3000/api/book/getAll");
+      const books = response.data;
 
-    const paths = books.map((book) => ({
-      params: { seriesId: book._id },
-    }));
+      const paths = books.map((book) => ({
+         params: { seriesId: book._id },
+      }));
 
-    return {
-      paths,
-      fallback: true,
-    };
-  } catch (error) {
-    console.error('Error fetching books:', error);
-    return {
-      paths: [],
-      fallback: true,
-    };
-  }
+      return {
+         paths,
+         fallback: true,
+      };
+   } catch (error) {
+      console.error("Error fetching books:", error);
+      return {
+         paths: [],
+         fallback: true,
+      };
+   }
 }
 
 export async function getStaticProps({ params }) {
-  const { seriesId } = params;
+   const { seriesId } = params;
 
-  try {
-    const response = await axios.get(`http://localhost:3000/api/book/getById/${seriesId}`);
-    const book = response.data;
+   try {
+      const response = await axios.get(
+         `http://localhost:3000/api/book/getById/${seriesId}`
+      );
+      const book = response.data;
 
-    return {
-      props: {
-        book,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching book:', error);
+      return {
+         props: {
+            book,
+         },
+      };
+   } catch (error) {
+      console.error("Error fetching book:", error);
 
-    return {
-      props: {
-        book: null,
-      },
-    };
-  }
+      return {
+         props: {
+            book: null,
+         },
+      };
+   }
 }
