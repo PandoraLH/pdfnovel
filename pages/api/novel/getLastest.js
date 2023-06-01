51685f9b052dc608c42f972ecbf08cae9d808725
PandoraLH <lixhen2002@gmail.com>
2023-06-01 19:32:32 +0700

    homepage

---
+++ pages/api/novel/getLastest.js
@@ -0,0 +1,22 @@
+import db from "@/utils/db";
+import { data } from "@/models/data";
+
+export default async function handler(req, res) {
+   if (req.method === "GET") {
+      try {
+         await db.connect();
+
+         const latestNovels = await data
+            .find()
+            .sort({ updatedAt: -1 }) // Sort by updatedAt field in descending order
+            .limit(30) // Retrieve the latest 30 novels
+            .exec();
+
+         res.status(200).json(latestNovels);
+      } catch (error) {
+         res.status(500).json({ error: "Failed to retrieve latest novels" });
+      } finally {
+         await db.disconnect();
+      }
+   }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,38 @@
 import Layout from "@/Layout/Layout";
+import MyCarousel from "components/HomePage/MyCarousel";
 import Image from "next/image";
+import axios from "axios";
+import { Box, Button } from "@mui/material";
+import ListNovel from "components/HomePage/ListNovel";
 
-export default function Home() {
+export default function HomePage({ latestNovels }) {
    return (
-      <div>
-         <Image src="/cover.jpg" width={1152} height={150} />
-      </div>
+      <Box className="bg-zinc-100">
+         <Image src="/cover.jpg" width={1152} height={200} />
+         <MyCarousel novels={latestNovels} />
+         <ListNovel novels={latestNovels} />
+      </Box>
    );
 }
 
-Home.getInitialProps = async () => {
-   return {
-      title: "Home", // Provide the title as a prop in getInitialProps
-   };
-};
+export async function getServerSideProps() {
+   try {
+      const response = await axios.get(
+         "http://localhost:3000/api/novel/getLastest"
+      );
+      const latestNovels = response.data;
+
+      return {
+         props: {
+            latestNovels,
+         },
+      };
+   } catch (error) {
+      console.error("Failed to fetch latest novels:", error);
+      return {
+         props: {
+            latestNovels: null,
+         },
+      };
+   }
+}
